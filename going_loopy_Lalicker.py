5a42cfefbddaf66d596c32b4763cc8d5dd300d41
jacob <lalickerj28@cbchs.org>
2024-09-20 11:16:27 -0500

    completed going loopy

---
+++ going_loopy_Lalicker.py
@@ -0,0 +1,11 @@
+weight = 165
+for year in range(1, 16):
+    weight = weight + 1
+    moon_weight = weight * 0.165
+    print(f"Year {year} is {moon_weight}")
+
+x = 1
+results = ['bacon,' 'lettuce,' 'tomatos,' 'cheese,' 'mustard']
+for i in results:
+    print(f"(x) {i}")
+    x = x + 1
